7d5acde7b360187ceffa76c1d436d0e3d400e129
zhangw <zhang9748@foxmail.com>
2025-08-03 08:50:04 +0800

    Update tempture.py

--- week1/tempture.py
+++ week1/tempture.py
@@ -2,8 +2,9 @@ import numpy as np
 
 temptures = np.array([18.5, 19, 20, 25.0, 2, 30, 13.9])
 
+print('temptures:', temptures)
 print('average:',np.mean(temptures))
 print('lowest:', np.min(temptures))
 print('highest:',np.max(temptures))
 print('ftemptures', temptures * 1.8 + 32)
-print('daysExcced20', np.where(temptures >= 20))
+print('daysExcced20', np.where(temptures >= 20)[0]+1)
