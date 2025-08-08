edefec28ac86f0a100852ceee2defb85212ced11
zhangw <zhang9748@foxmail.com>
2025-08-09 05:01:00 +0800

    feat: add lives

--- week2/func.py
+++ week2/func.py
@@ -13,19 +13,27 @@ def main():
     # 这里写主要的程序逻辑
     word = randomWord()
     blanks = blanks = word.__len__() * '_'
+    lives = 5
 
     while True:
         print('please guess the word, one letter each time: ', blanks)
         letter = input()
         if letter.__len__() != 1 or letter == '':
+            print('invalid input')
             continue
         if letter in word:
             for i in range(word.__len__()):
                 if word[i] == letter:
                     blanks = blanks[:i] + letter + blanks[i+1:]
-        
+            print('remain lives:', lives)
+        else:
+            lives -= 1
+            print('remain lives', lives)
         if blanks == word:
             print('you win, word is:',  word)
+            break
+        if lives <= 0:
+            print('you lose, word is:', word)
             break
 
 
