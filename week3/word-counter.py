65a34d2d6b0bf67d106a4f9db78083a2a8f5fa2a
zhangw <zhang9748@foxmail.com>
2025-08-16 05:35:44 +0800

    feat: update

--- week3/word-counter.py
+++ week3/word-counter.py
@@ -1,5 +1,3 @@
-import os
-
 def words_of_line(line: str):
     words_count = line.strip().split()
     return words_count.__len__()
@@ -8,21 +6,13 @@ class FileOperator:
     def __init__(self, file_path: str):
         self.file_path = file_path
 
-
     def read_and_display(self):
-        print(f"\n--- Content of {self.file_path} ---")
-        try:
-            with open(self.file_path, 'r', encoding='utf-8') as f:
-                lines = f.readlines()
-                count = 0
-                for line in lines:
-                    count += words_of_line(line)
-                print('words of the file:', count)
-        except FileNotFoundError:
-            print(f"Error: The file '{self.file_path}' was not found.")
-        except Exception as e:
-            print(f"An unexpected error occurred while reading: {e}")
-
+        with open(self.file_path, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            count = 0
+            for line in lines:
+                count += words_of_line(line)
+            print('words of the file:', count)
 
 def main():
     file_path = 'week3/demo.txt'
